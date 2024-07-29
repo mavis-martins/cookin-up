460037f089a8117b121655a2bac2cba6933edab0
Mavis <mavisdeving@gmail.com>
2024-07-29 08:53:14 -0300

    first commit

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,120 @@
+<script lang="ts">
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import Tag from './Tag.vue';
+import MostrarReceitas from './MostrarReceitas.vue';
+
+type Pagina = 'Selecionar Ingredientes' | 'Mostrar Receitas';
+
+export default {
+    data() {
+        return {
+            ingredientes: [] as string[],
+            conteudo:  'Selecionar Ingredientes' as Pagina
+        }
+    },
+    components: {
+        SelecionarIngredientes,
+        Tag,
+        MostrarReceitas
+    },
+    methods: {
+        adicionarIngredientes(ingrediente: string) {
+            this.ingredientes.push(ingrediente);
+        },
+        removerIngrediente(ingrediente: string) {
+            this.ingredientes = this.ingredientes.filter(item => item !== ingrediente);
+        },
+        navegarPagina(pagina: Pagina) {
+            this.conteudo = pagina;
+        }
+    }
+}
+</script>
+
+<template>
+    <main class="conteudo-principal">
+        <section>
+            <span class="subtitulo-lg sua-lista-texto">
+                Sua lista:
+            </span>
+
+            <ul v-if="ingredientes.length > 0" class="ingredientes-sua-lista">
+                <li v-for="ingrediente in ingredientes" :key="ingrediente">
+                    <Tag :texto="ingrediente" ativa />
+                </li>
+            </ul>
+
+            <p v-else class="paragrafo lista-vazia">
+                <img src="../assets/imagens/icones/lista-vazia.svg" alt="Ícone de Pesquisa">
+                Sua lista está vazia, selecione ingredientes para iniciar.
+            </p>
+        </section>
+
+        <KeepAlive include="SelecionarIngredientes">
+            <SelecionarIngredientes v-if="conteudo === 'Selecionar Ingredientes'"
+                @adicionarIngrediente="adicionarIngredientes"
+                @removerIngrediente="removerIngrediente"
+                @buscarReceitas="navegarPagina('Mostrar Receitas')"
+            />
+
+            <MostrarReceitas 
+                v-else-if="conteudo === 'Mostrar Receitas'" 
+                :ingredientes="ingredientes"
+                @editarLista="navegarPagina('Selecionar Ingredientes')"
+            />
+        </KeepAlive>
+    </main>
+</template>
+
+<style scoped>
+.conteudo-principal {
+    padding: 6.5rem 7.5rem;
+    border-radius: 3.75rem 3.75rem 0rem 0rem;
+    background: var(--creme, #FFFAF3);
+    color: var(--cinza, #444);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5rem;
+}
+
+.sua-lista-texto {
+    color: var(--coral, #F0633C);
+    display: block;
+    text-align: center;
+    margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+    display: flex;
+    justify-content: center;
+    gap: 1rem 1.5rem;
+    flex-wrap: wrap;
+}
+
+.lista-vazia {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 0.25rem;
+
+    color: var(--coral, #F0633C);
+    text-align: center;
+}
+
+@media only screen and (max-width: 1300px) {
+    .conteudo-principal {
+        padding: 5rem 3.75rem;
+        gap: 3.5rem;
+    }
+}
+
+@media only screen and (max-width: 767px) {
+    .conteudo-principal {
+        padding: 4rem 1.5rem;
+        gap: 4rem;
+    }
+}
+</style>
